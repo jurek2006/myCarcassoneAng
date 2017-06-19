8c38383dbf5437808d7c5cd9accad3fa86729919
Jurek <jurek2006@gmail.com>
2017-06-19 09:23:53 +0200

    Working very simpliefied one-dimension array of very simpliefied CS tiles (not acting like real CS board yet)

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Tile } from './tile.model';
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+	board: Tile[] = [new Tile(0,'red', false), new Tile(1,'blue', true) ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  tileClicked(boardTile: Tile){
+  	console.log(boardTile);
+  	if(boardTile.statusActive){
+  	  	this.board.push(new Tile(2,'yellow', false));
+  	}
   }
 
 }
---
+++ src/app/board/tile.model.ts
@@ -0,0 +1,4 @@
+export class Tile{
+
+	constructor(public number: Number, public color: string, public statusActive: boolean = false, public tileRef: Number = null){	}
+}
